f03e5e468db47b524ca4dad4990d8a2d10e156a3
Geist5000 <geist5000@gmail.com>
2022-04-01 08:34:09 +0200

    removed imports

--- src/de/geist5000/sort_examples/utils/DataSource.java
+++ src/de/geist5000/sort_examples/utils/DataSource.java
@@ -1,37 +1,35 @@
 package de.geist5000.sort_examples.utils;
 
-import java.util.AbstractCollection;
+
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
 
 public class DataSource<T> {
     T[] data;
 
     public DataSource(T[] data) {
-        this.data = Arrays.copyOf(data,data.length);
+        this.data = Arrays.copyOf(data, data.length);
     }
 
-    public T get(int index){
+    public T get(int index) {
         return data[index];
     }
 
-    public T set(int index, T value){
+    public T set(int index, T value) {
         return data[index] = value;
     }
 
     public void swapEntries(int first, int second) {
         T tmp = this.get(first);
-        this.set(first,this.get(second));
-        this.set(second,tmp);
+        this.set(first, this.get(second));
+        this.set(second, tmp);
     }
 
-    public int size(){
+    public int size() {
         return data.length;
     }
 
 
-    public T[] getRaw(){
+    public T[] getRaw() {
         return data;
     }
 }
